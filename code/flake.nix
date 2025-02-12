93151920f53a575de5d7d3da73d80d4da5dca133
HuyDNA <huydo862003@gmail.com>
2025-02-12 22:19:17 +0700

    chore: init flake

---
+++ code/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          mpi
+          cmake
+          gnumake
+          gcc
+        ];
+        CMAKE_EXPORT_COMPILE_COMMANDS = 1;
+      };
+    });
+}
