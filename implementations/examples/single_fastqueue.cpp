52b4c946b584378bb462e0c0cdb611381e022b25
HuyDNA <huydo862003@gmail.com>
2025-03-11 15:24:13 +0700

    feat: add examples

---
+++ implementations/examples/single_fastqueue.cpp
@@ -0,0 +1,31 @@
+#include "../bcl/fastqueue.hpp"
+#include <cstdio>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  int rank;
+  int size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0) {
+    FqDequeuer<int> queue(1000, 0, rank, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = 0; i < 50 * (size - 1); ++i) {
+      int value;
+      if (queue.dequeue(&value)) {
+        printf("dequeue %d\n", value);
+      } else {
+        printf("dequeue NULL\n");
+      }
+    }
+  } else {
+    FqEnqueuer<int> queue(1000, 0, rank, MPI_COMM_WORLD);
+    for (int i = 0; i < 50; ++i) {
+      queue.enqueue(i);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+}
