58937c71ad096ff24d8950d207e1b6f9b1c95d41
HuyDNA <huydo862003@gmail.com>
2025-03-20 09:56:42 +0700

    feat: add examples for slot-queue

---
+++ implementations/examples/batch_slotqueue.cpp
@@ -0,0 +1,33 @@
+#include "../slot-queue/slot-queue.hpp"
+#include <cstdio>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  int rank;
+  int size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0) {
+    SlotDequeuer<int> queue(1000, rank, rank, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = 0; i < 50 * (size - 1); ++i) {
+      int value;
+      if (queue.dequeue(&value)) {
+        printf("dequeue %d\n", value);
+      } else {
+        printf("dequeue NULL\n");
+      }
+    }
+  } else {
+    SlotEnqueuer<int> queue(1000, 0, rank, MPI_COMM_WORLD);
+    std::vector<int> buffer;
+    for (int i = 0; i < 50; ++i) {
+      buffer.push_back(i);
+    }
+    queue.enqueue(buffer);
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
+}
---
+++ implementations/examples/batch_slotqueue_overrun.cpp
@@ -0,0 +1,33 @@
+#include "../slot-queue/slot-queue.hpp"
+#include <cstdio>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  int rank;
+  int size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0) {
+    SlotDequeuer<int> queue(25, 0, rank, MPI_COMM_WORLD);
+    for (int i = 0; i < 1000; ++i) {
+      int value;
+      if (queue.dequeue(&value)) {
+        printf("dequeue %d\n", value);
+      }
+    }
+  } else {
+    SlotEnqueuer<int> queue(25, 0, rank, MPI_COMM_WORLD);
+    std::vector<int> buffer;
+    for (int i = 0; i < 60; ++i) {
+      buffer.push_back(i);
+      if (buffer.size() == 20) {
+        while (!queue.enqueue(buffer))
+          ;
+        buffer.clear();
+      }
+    }
+  }
+
+  MPI_Finalize();
+}
