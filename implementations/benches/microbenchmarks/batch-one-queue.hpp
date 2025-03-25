7e1ae48b1631993da699e6b0726726b489ba533b
HuyDNA <huydo862003@gmail.com>
2025-03-25 21:22:48 +0700

    feat: add batch enqueue benchmarks

---
+++ implementations/benches/microbenchmarks/batch-one-queue.hpp
@@ -0,0 +1,372 @@
+#pragma once
+
+#include "bcl/fastqueue.hpp"
+#include "benches/utils.h"
+#include "ltqueue/ltqueue.hpp"
+#include "slot-queue/slot-queue.hpp"
+#include <chrono>
+#include <mpi.h>
+#include <thread>
+
+inline void
+slotqueue_batch_one_queue_microbenchmark(unsigned long long number_of_elements,
+                                          int batch_size, int iterations = 10) {
+  int size;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  unsigned long long elements_per_queue = number_of_elements / (size - 1) + 1;
+
+  double total_enqueues = 0;
+  double total_dequeues = 0;
+  double total_successful_enqueues = 0;
+  double total_successful_dequeues = 0;
+  double total_microseconds = 0;
+  double total_enqueues_microseconds = 0;
+  double total_dequeues_microseconds = 0;
+
+  for (int i = 0; i < iterations; ++i) {
+    double local_enqueues = 0;
+    double local_dequeues = 0;
+    double local_successful_enqueues = 0;
+    double local_successful_dequeues = 0;
+    double local_microseconds = 0;
+    double local_enqueues_microseconds = 0;
+    double local_dequeues_microseconds = 0;
+
+    if (rank == 0) {
+      SlotDequeuer<int> queue(elements_per_queue, rank, rank, MPI_COMM_WORLD);
+      std::this_thread::sleep_for(std::chrono::microseconds(100));
+      auto t1 = std::chrono::high_resolution_clock::now();
+      while (local_successful_dequeues < number_of_elements) {
+        int output;
+        if (queue.dequeue(&output)) {
+          ++local_dequeues;
+          ++local_successful_dequeues;
+        } else {
+          ++local_dequeues;
+        }
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_dequeues_microseconds = local_microseconds;
+    } else {
+      SlotEnqueuer<int> queue(elements_per_queue, 0, rank, MPI_COMM_WORLD);
+      std::vector<int> batch;
+      batch.reserve(batch_size);
+      auto t1 = std::chrono::high_resolution_clock::now();
+      for (unsigned long long i = 0; i < elements_per_queue; ++i) {
+        batch.push_back(i);
+        if (batch.size() == batch_size) {
+          if (queue.enqueue(batch)) {
+            ++local_enqueues;
+            ++local_successful_enqueues;
+          } else {
+            ++local_enqueues;
+          }
+          batch.clear();
+        }
+      }
+      if (batch.size() > 0) {
+        if (queue.enqueue(batch)) {
+          ++local_enqueues;
+          ++local_successful_enqueues;
+        } else {
+          ++local_enqueues;
+        }
+        batch.clear();
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_enqueues_microseconds = local_microseconds;
+    }
+
+    double enqueues = 0;
+    double dequeues = 0;
+    double successful_enqueues = 0;
+    double successful_dequeues = 0;
+    double microseconds = 0;
+    double enqueues_microseconds = 0;
+    double dequeues_microseconds = 0;
+
+    MPI_Allreduce(&local_dequeues, &dequeues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues, &enqueues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_dequeues, &successful_dequeues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_enqueues, &successful_enqueues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_microseconds, &microseconds, 1, MPI_DOUBLE, MPI_MAX,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues_microseconds, &enqueues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_dequeues_microseconds, &dequeues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    total_enqueues += enqueues;
+    total_dequeues += dequeues;
+    total_successful_dequeues += successful_dequeues;
+    total_successful_enqueues += successful_enqueues;
+    total_microseconds += microseconds;
+    total_enqueues_microseconds += enqueues_microseconds;
+    total_dequeues_microseconds += dequeues_microseconds;
+  }
+
+  report("Slotqueue", number_of_elements, iterations, total_microseconds,
+         total_dequeues, total_successful_dequeues, total_dequeues_microseconds,
+         total_enqueues, total_successful_enqueues,
+         total_enqueues_microseconds);
+}
+
+inline void
+ltqueue_batch_one_queue_microbenchmark(unsigned long long number_of_elements,
+                                        int batch_size, int iterations = 10) {
+  int size;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  unsigned long long elements_per_queue = number_of_elements / (size - 1) + 1;
+
+  double total_enqueues = 0;
+  double total_dequeues = 0;
+  double total_successful_enqueues = 0;
+  double total_successful_dequeues = 0;
+  double total_microseconds = 0;
+  double total_enqueues_microseconds = 0;
+  double total_dequeues_microseconds = 0;
+
+  for (int i = 0; i < iterations; ++i) {
+    double local_enqueues = 0;
+    double local_dequeues = 0;
+    double local_successful_enqueues = 0;
+    double local_successful_dequeues = 0;
+    double local_microseconds = 0;
+    double local_enqueues_microseconds = 0;
+    double local_dequeues_microseconds = 0;
+
+    if (rank == 0) {
+      LTDequeuer<int> queue(elements_per_queue, rank, rank, MPI_COMM_WORLD);
+      std::this_thread::sleep_for(std::chrono::microseconds(100));
+      auto t1 = std::chrono::high_resolution_clock::now();
+      while (local_successful_dequeues < number_of_elements) {
+        int output;
+        if (queue.dequeue(&output)) {
+          ++local_dequeues;
+          ++local_successful_dequeues;
+        } else {
+          ++local_dequeues;
+        }
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_dequeues_microseconds = local_microseconds;
+    } else {
+      LTEnqueuer<int> queue(elements_per_queue, 0, rank, MPI_COMM_WORLD);
+      std::vector<int> batch;
+      batch.reserve(batch_size);
+      auto t1 = std::chrono::high_resolution_clock::now();
+      for (unsigned long long i = 0; i < elements_per_queue; ++i) {
+        batch.push_back(i);
+        if (batch.size() == batch_size) {
+          if (queue.enqueue(batch)) {
+            ++local_enqueues;
+            ++local_successful_enqueues;
+          } else {
+            ++local_enqueues;
+          }
+          batch.clear();
+        }
+      }
+      if (batch.size() > 0) {
+        if (queue.enqueue(batch)) {
+          ++local_enqueues;
+          ++local_successful_enqueues;
+        } else {
+          ++local_enqueues;
+        }
+        batch.clear();
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_enqueues_microseconds = local_microseconds;
+    }
+
+    double enqueues = 0;
+    double dequeues = 0;
+    double successful_enqueues = 0;
+    double successful_dequeues = 0;
+    double microseconds = 0;
+    double enqueues_microseconds = 0;
+    double dequeues_microseconds = 0;
+
+    MPI_Allreduce(&local_dequeues, &dequeues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues, &enqueues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_dequeues, &successful_dequeues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_enqueues, &successful_enqueues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_microseconds, &microseconds, 1, MPI_DOUBLE, MPI_MAX,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues_microseconds, &enqueues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_dequeues_microseconds, &dequeues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    total_enqueues += enqueues;
+    total_dequeues += dequeues;
+    total_successful_dequeues += successful_dequeues;
+    total_successful_enqueues += successful_enqueues;
+    total_microseconds += microseconds;
+    total_enqueues_microseconds += enqueues_microseconds;
+    total_dequeues_microseconds += dequeues_microseconds;
+  }
+
+  report("LTQueue", number_of_elements, iterations, total_microseconds,
+         total_dequeues, total_successful_dequeues, total_dequeues_microseconds,
+         total_enqueues, total_successful_enqueues,
+         total_enqueues_microseconds);
+}
+
+inline void
+fastqueue_batch_one_queue_microbenchmark(unsigned long long number_of_elements,
+                                          int batch_size, int iterations = 10) {
+  int size;
+  int rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  unsigned long long elements_per_queue = number_of_elements / (size - 1) + 1;
+
+  double total_enqueues = 0;
+  double total_dequeues = 0;
+  double total_successful_enqueues = 0;
+  double total_successful_dequeues = 0;
+  double total_microseconds = 0;
+  double total_enqueues_microseconds = 0;
+  double total_dequeues_microseconds = 0;
+
+  for (int i = 0; i < iterations; ++i) {
+    double local_enqueues = 0;
+    double local_dequeues = 0;
+    double local_successful_enqueues = 0;
+    double local_successful_dequeues = 0;
+    double local_microseconds = 0;
+    double local_enqueues_microseconds = 0;
+    double local_dequeues_microseconds = 0;
+
+    if (rank == 0) {
+      FqDequeuer<int> queue(number_of_elements, rank, rank, MPI_COMM_WORLD);
+      std::this_thread::sleep_for(std::chrono::microseconds(100));
+      auto t1 = std::chrono::high_resolution_clock::now();
+      while (local_successful_dequeues < number_of_elements) {
+        int output;
+        if (queue.dequeue(&output)) {
+          ++local_dequeues;
+          ++local_successful_dequeues;
+        } else {
+          ++local_dequeues;
+        }
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_dequeues_microseconds = local_microseconds;
+    } else {
+      FqEnqueuer<int> queue(number_of_elements, 0, rank, MPI_COMM_WORLD);
+      std::vector<int> batch;
+      batch.reserve(batch_size);
+      auto t1 = std::chrono::high_resolution_clock::now();
+      for (unsigned long long i = 0; i < elements_per_queue; ++i) {
+        batch.push_back(i);
+        if (batch.size() == batch_size) {
+          if (queue.enqueue(batch)) {
+            ++local_enqueues;
+            ++local_successful_enqueues;
+          } else {
+            ++local_enqueues;
+          }
+          batch.clear();
+        }
+      }
+      if (batch.size() > 0) {
+        if (queue.enqueue(batch)) {
+          ++local_enqueues;
+          ++local_successful_enqueues;
+        } else {
+          ++local_enqueues;
+        }
+        batch.clear();
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      local_microseconds =
+          std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1)
+              .count();
+      local_enqueues_microseconds = local_microseconds;
+    }
+
+    double enqueues = 0;
+    double dequeues = 0;
+    double successful_enqueues = 0;
+    double successful_dequeues = 0;
+    double microseconds = 0;
+    double enqueues_microseconds = 0;
+    double dequeues_microseconds = 0;
+
+    MPI_Allreduce(&local_dequeues, &dequeues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues, &enqueues, 1, MPI_DOUBLE, MPI_SUM,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_dequeues, &successful_dequeues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_successful_enqueues, &successful_enqueues, 1,
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_microseconds, &microseconds, 1, MPI_DOUBLE, MPI_MAX,
+                  MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_enqueues_microseconds, &enqueues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    MPI_Allreduce(&local_dequeues_microseconds, &dequeues_microseconds, 1,
+                  MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+    total_enqueues += enqueues;
+    total_dequeues += dequeues;
+    total_successful_dequeues += successful_dequeues;
+    total_successful_enqueues += successful_enqueues;
+    total_microseconds += microseconds;
+    total_enqueues_microseconds += enqueues_microseconds;
+    total_dequeues_microseconds += dequeues_microseconds;
+  }
+
+  report("FastQueue", number_of_elements, iterations, total_microseconds,
+         total_dequeues, total_successful_dequeues, total_dequeues_microseconds,
+         total_enqueues, total_successful_enqueues,
+         total_enqueues_microseconds);
+}
--- implementations/main.cpp
+++ implementations/main.cpp
@@ -1,4 +1,5 @@
 #include "benches/microbenchmarks/single-one-queue.hpp"
+#include "benches/microbenchmarks/batch-one-queue.hpp"
 #include <comm.hpp>
 #include <ltqueue/ltqueue.hpp>
 #include <mpi.h>
@@ -6,9 +7,9 @@
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
 
-  slotqueue_single_one_queue_microbenchmark(1000000);
-  ltqueue_single_one_queue_microbenchmark(1000000);
-  fastqueue_single_one_queue_microbenchmark(1000000);
+  slotqueue_batch_one_queue_microbenchmark(1000000, 1000);
+  ltqueue_batch_one_queue_microbenchmark(1000000, 1000);
+  fastqueue_batch_one_queue_microbenchmark(1000000, 1000);
 
   MPI_Finalize();
 }
