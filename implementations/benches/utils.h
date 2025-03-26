941f47257ebaacbad6f8b999b3dc5fcc3749457b
HuyDNA <huydo862003@gmail.com>
2025-03-26 09:26:28 +0700

    fix: more precise latency

--- implementations/benches/utils.h
+++ implementations/benches/utils.h
@@ -16,11 +16,11 @@ inline void report(const std::string &title,
   if (rank == 0) {
     printf("---- %s ----\n", title.c_str());
     printf("Dequeue latency: %g us\n",
-           dequeue_microseconds / number_of_elements / iterations);
+           dequeue_microseconds / successful_dequeues);
     printf("Dequeue throughput: %g 10^5ops/s\n",
            successful_dequeues / dequeue_microseconds * 10);
     printf("Enqueue latency: %g us\n",
-           enqueue_latency_microseconds / number_of_elements / iterations);
+           enqueue_latency_microseconds / successful_enqueues);
     printf("Enqueue throughput: %g 10^5ops/s\n",
            successful_enqueues / enqueue_microseconds * 10);
     printf("Total throughput: %g 10^5ops/s\n",
