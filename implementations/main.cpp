69ae556de8c093990093664e18a751a5b17d6c59
HuyDNA <huydo862003@gmail.com>
2025-03-26 10:36:17 +0700

    feat: add caliper init

--- implementations/main.cpp
+++ implementations/main.cpp
@@ -1,10 +1,12 @@
 #include "benches/microbenchmarks/single-one-queue.hpp"
+#include <caliper/cali.h>
 #include <comm.hpp>
 #include <ltqueue/ltqueue.hpp>
 #include <mpi.h>
 
 int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
+  CALI_CXX_MARK_FUNCTION;
 
   slotqueue_single_one_queue_microbenchmark(1000000);
   ltqueue_single_one_queue_microbenchmark(1000000);
