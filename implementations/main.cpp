a5271c1b4f2c113c2bf4ed05cc2a3f5b8b3ed423
HuyDNA <huydo862003@gmail.com>
2025-04-28 17:55:20 +0700

    bench(ccqueue): add benchmark for ccqueue

--- implementations/main.cpp
+++ implementations/main.cpp
@@ -73,6 +73,7 @@ int main(int argc, char **argv) {
     slotqueueV2c_single_one_queue_microbenchmark(100000, 5);
     ltqueue_single_one_queue_microbenchmark(100000, 5);
     fastqueue_single_one_queue_microbenchmark(100000, 5);
+    ccqueue_single_one_queue_microbenchmark(100000, 5);
   }
 
   if (run_bench) {
