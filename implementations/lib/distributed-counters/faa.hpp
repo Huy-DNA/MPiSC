fe0e19de4af5e7326b21dfc2fb08ba6a95caa67e
Huy-DNA <huydo862003@gmail.com>
2025-06-01 21:16:39 +0700

    fix(faa): add move semantics

--- implementations/lib/distributed-counters/faa.hpp
+++ implementations/lib/distributed-counters/faa.hpp
@@ -1,14 +1,12 @@
 #pragma once
-
 #include "../comm.hpp"
 #include <mpi.h>
 
 class FaaCounter {
 private:
-  MPI_Aint *_counter_ptr;
-  MPI_Win _counter_win;
-
-  MPI_Info _info;
+  MPI_Aint *_counter_ptr = nullptr;
+  MPI_Win _counter_win = MPI_WIN_NULL;
+  MPI_Info _info = MPI_INFO_NULL;
   MPI_Aint _host;
 
 public:
@@ -17,10 +15,8 @@ public:
     MPI_Info_set(this->_info, "same_disp_unit", "true");
     MPI_Info_set(this->_info, "accumulate_ordering", "none");
     this->_host = host;
-
     int rank;
     MPI_Comm_rank(comm, &rank);
-
     if (host == rank) {
       MPI_Win_allocate(sizeof(MPI_Aint), sizeof(MPI_Aint), this->_info, comm,
                        &this->_counter_ptr, &this->_counter_win);
@@ -32,17 +28,31 @@ public:
     if (host == rank) {
       *this->_counter_ptr = 0;
     }
-
     MPI_Win_flush_all(this->_counter_win);
     MPI_Barrier(comm);
     MPI_Win_flush_all(this->_counter_win);
   }
+
   FaaCounter(const FaaCounter &) = delete;
   FaaCounter &operator=(const FaaCounter &) = delete;
+
+  FaaCounter(FaaCounter &&other) noexcept
+      : _counter_ptr(other._counter_ptr), _counter_win(other._counter_win),
+        _info(other._info), _host(other._host) {
+    other._counter_ptr = nullptr;
+    other._counter_win = MPI_WIN_NULL;
+    other._info = MPI_INFO_NULL;
+    other._host = 0;
+  }
+
   ~FaaCounter() {
-    MPI_Win_unlock_all(this->_counter_win);
-    MPI_Win_free(&this->_counter_win);
-    MPI_Info_free(&this->_info);
+    if (_counter_win != MPI_WIN_NULL) {
+      MPI_Win_unlock_all(this->_counter_win);
+      MPI_Win_free(&this->_counter_win);
+    }
+    if (_info != MPI_INFO_NULL) {
+      MPI_Info_free(&this->_info);
+    }
   }
 
   inline MPI_Aint get_and_increment() {
