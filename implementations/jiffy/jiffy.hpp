c7f9e6d39b92204d0eb6a06c1d032520affe083e
Huy-DNA <huydo862003@gmail.com>
2025-06-03 18:02:10 +0700

    feat(jiffy): implement enqueue

--- implementations/jiffy/jiffy.hpp
+++ implementations/jiffy/jiffy.hpp
@@ -29,6 +29,39 @@ private:
   bclx::gptr<bclx::gptr<segment_t>> _tail_of_queue = nullptr;
   bclx::gptr<segment_t> _head_of_queue = nullptr;
 
+  bclx::gptr<segment_t> allocate_segment(int pos_in_queue) {
+    bclx::gptr<segment_t> segment = BCL::alloc<segment_t>(1);
+
+    segment.local()->curr_data_buffer = BCL::alloc<T>(SEGMENT_SIZE);
+
+    segment.local()->curr_status_buffer = BCL::alloc<status_t>(SEGMENT_SIZE);
+    for (int i = 0; i < SEGMENT_SIZE; ++i) {
+      segment.local()->curr_status_buffer.local()[i] = EMPTY;
+    }
+
+    segment.local()->next = BCL::alloc<bclx::gptr<segment_t>>(1);
+    *segment.local()->next.local() = nullptr;
+
+    segment.local()->prev = BCL::alloc<bclx::gptr<segment_t>>(1);
+    *segment.local()->prev.local() = nullptr;
+
+    segment.local()->head = BCL::alloc<int>(1);
+    *segment.local()->head.local() = 0;
+
+    segment.local()->pos_in_queue = pos_in_queue;
+
+    return segment;
+  }
+
+  void _fully_reclaim_segment(bclx::gptr<segment_t> segment) {
+    BCL::dealloc(segment.local()->curr_data_buffer);
+    BCL::dealloc(segment.local()->curr_status_buffer);
+    BCL::dealloc(segment.local()->next);
+    BCL::dealloc(segment.local()->prev);
+    BCL::dealloc(segment.local()->head);
+    BCL::dealloc(segment);
+  }
+
 public:
   JiffyQueue(int dequeuer_rank) {
     int self_rank = BCL::my_rank;
@@ -37,30 +70,10 @@ public:
       *this->_tail = 0;
 
       this->_tail_of_queue = BCL::alloc<bclx::gptr<segment_t>>(1);
-      *this->_tail_of_queue = BCL::alloc<segment_t>(1);
-      this->_tail_of_queue.local()->local()->curr_data_buffer =
-          BCL::alloc<T>(SEGMENT_SIZE);
-      this->_tail_of_queue.local()->local()->curr_status_buffer =
-          BCL::alloc<status_t>(SEGMENT_SIZE);
-      for (int i = 0; i < SEGMENT_SIZE; ++i) {
-        this->_tail_of_queue.local()->local()->curr_status_buffer.local()[i] =
-            EMPTY;
-      }
-      this->_tail_of_queue.local()->local()->next =
-          BCL::alloc<bclx::gptr<segment_t>>(1);
-      *this->_tail_of_queue.local()->local()->next.local() = nullptr;
-      this->_tail_of_queue.local()->local()->prev =
-          BCL::alloc<bclx::gptr<segment_t>>(1);
-      *this->_tail_of_queue.local()->local()->prev.local() = nullptr;
-      this->_tail_of_queue.local()->local()->head = BCL::alloc<int>(1);
-      *this->_tail_of_queue.local()->local()->head.local() = 0;
-      this->_tail_of_queue.local()->local()->pos_in_queue = 0;
-
-      this->_head_of_queue = *this->_tail_of_queue.local();
+      *this->_tail_of_queue = allocate_segment(0);
 
       BCL::broadcast(_tail, self_rank);
       BCL::broadcast(_tail_of_queue, self_rank);
-
     } else {
       this->_tail = BCL::broadcast(_tail, dequeuer_rank);
       this->_tail_of_queue = BCL::broadcast(_tail_of_queue, dequeuer_rank);
@@ -78,11 +91,46 @@ public:
     other._head_of_queue = nullptr;
   }
 
-  ~JiffyQueue() {}
+  ~JiffyQueue() {
+    // free later...
+  }
 
   bool enqueue(const T &data) {
+    int location;
+    int inc = 1;
+    bclx::fetch_and_op_sync(this->_tail, &inc, BCL::plus<int>{}, &location);
+
+    while (true) {
+      bclx::gptr<segment_t> last_segment_ptr =
+          bclx::aread_sync(this->_tail_of_queue);
+      segment_t last_segment = bclx::aread_sync(last_segment_ptr);
+      if ((last_segment.pos_in_queue + 1) * SEGMENT_SIZE > location) {
+        break;
+      }
+      bclx::gptr<segment_t> new_last =
+          allocate_segment(last_segment.pos_in_queue + 1);
+      *new_last.local()->prev.local() = last_segment_ptr;
+      bclx::gptr<segment_t> old_tail_of_queue;
+      bclx::compare_and_swap_sync(this->_tail_of_queue, &last_segment_ptr,
+                                  &new_last, &old_tail_of_queue);
+      if (old_tail_of_queue != last_segment_ptr) {
+        this->_fully_reclaim_segment(new_last);
+      } else {
+        bclx::aput_sync(new_last, last_segment.next);
+      }
+    }
+
+    bclx::gptr<segment_t> temp_tail_ptr = bclx::aget_sync(this->_tail_of_queue);
+    segment_t temp_tail = bclx::aget_sync(temp_tail_ptr);
+    while (bclx::aget_sync(temp_tail_ptr.head) > location) {
+      temp_tail_ptr = bclx::aget_sync(temp_tail.prev);
+      temp_tail = bclx::aget_sync(temp_tail_ptr);
+    }
+
+    bclx::aput_sync(data, temp_tail.curr_data_buffer + location);
+    bclx::aput_sync(SET, temp_tail.curr_status_buffer + location);
+    return true;
   }
 
-  bool dequeue(T *output) {
-  }
+  bool dequeue(T *output) {}
 };
