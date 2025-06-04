06dbe69f8e462e69b4f96328a8aa6dd7f0f462b2
Huy-DNA <huydo862003@gmail.com>
2025-06-04 08:33:40 +0700

    fix(jiffy): example

--- implementations/examples/single_jiffy.cpp
+++ implementations/examples/single_jiffy.cpp
@@ -1,4 +1,4 @@
-#include "../jiffy/naive-jiffy.hpp"
+#include "../jiffy/jiffy.hpp"
 #include "bcl/backends/mpi/backend.hpp"
 #include <cstdio>
 #include <mpi.h>
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   if (rank == 0) {
-    NaiveJiffyQueue<int> queue(0);
+    JiffyQueue<int> queue(0);
     for (int i = 0; i < 50; ++i) {
       if (!queue.enqueue(i)) {
         printf("Enqueue failed \n");
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
       }
     }
   } else {
-    NaiveJiffyQueue<int> queue(0);
+    JiffyQueue<int> queue(0);
     for (int i = 0; i < 50; ++i) {
       if (!queue.enqueue(i)) {
         printf("Enqueue failed \n");
