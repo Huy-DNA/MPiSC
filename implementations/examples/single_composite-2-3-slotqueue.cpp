92030a87e923cbc6f5f8086b29875d7af7cd4ba5
Huy-DNA <huydo862003@gmail.com>
2025-06-28 10:15:43 +0700

    feat(23composite-slotqueue): add

---
+++ implementations/examples/single_composite-2-3-slotqueue.cpp
@@ -0,0 +1,37 @@
+#include "../slotqueue/composite-2-3-slotqueue.hpp"
+#include <cstdio>
+#include <mpi.h>
+
+int main(int argc, char **argv) {
+  MPI_Init(&argc, &argv);
+  int rank;
+  int size;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  if (rank == 0) {
+    Slot2C3Queue<int> queue(1000, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < 50; ++i) {
+      if (!queue.enqueue(i)) {
+        printf("Enqueue failed \n");
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (int i = 0; i < 50 * size; ++i) {
+      int value;
+      if (queue.dequeue(&value)) {
+        printf("dequeue %d\n", value);
+      } else {
+        printf("dequeue NULL\n");
+      }
+    }
+  } else {
+    Slot2C3Queue<int> queue(1000, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < 50; ++i) {
+      if (!queue.enqueue(i)) {
+        printf("Enqueue failed \n");
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+  MPI_Finalize();
+}
